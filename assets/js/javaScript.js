5c155c426c36454890ade0c8766ff97e4bbfa101
Tracy Tran <tracytran1189@gmail.com>
2021-12-18 21:52:23 -0600

    add current date

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -1,1 +1,10 @@
-var currentTime = { text: moment().format("h:00 A"), hour: moment().hour() };
+//Show current date and time
+
+$(document).ready(function() {
+    var todaySchedule = moment().format("MMM Do YYYY");
+    var currentHour = moment().format("HH");
+    var displayDate = document.getElementById("currentDay");
+    displayDate.innerHTML = todaySchedule;
+
+
+});
