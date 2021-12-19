9c1fc76233354f165b5c13ede2c6eea472809369
Tracy Tran <tracytran1189@gmail.com>
2021-12-18 22:03:09 -0600

    compare schedule time and current time

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -1,10 +1,25 @@
 //Show current date and time
+var currentHour = moment().format("HH");
 
 $(document).ready(function() {
     var todaySchedule = moment().format("MMM Do YYYY");
-    var currentHour = moment().format("HH");
     var displayDate = document.getElementById("currentDay");
     displayDate.innerHTML = todaySchedule;
+});
 
+//Compare each time slot to current time 
+$(".row").each(function() {
+    var timeSlot = $(this).attr("id").split("-")[1];
 
+    if (currentHour == timeSlot) {
+        $(this).addClass("present");
+        $(this).children(".description");
+
+    } else if (currentHour < timeSlot) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+    } else if (currentHour > timeSlot) {
+        $(this).removeClass("future");
+        $(this).addClass("past");
+    }
 });
